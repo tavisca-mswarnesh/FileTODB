43c27210e4ad807aa2005399582fbafdb678b718
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-12-08 00:02:46 +0530

    Add project files.

---
+++ FileToDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileToDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
