0d43dfdc4fc32cd68a77996916eaeda6d8a8e6fb
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-12-10 11:37:54 +0530

    All done

--- FileToDatabase/Program.cs
+++ FileToDatabase/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLayer;
+using DataAccessLayer.SQLDb;
+using System;
+using System.Diagnostics;
 
 namespace FileToDatabase
 {
@@ -6,7 +9,19 @@ namespace FileToDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sw = Stopwatch.StartNew();
+
+            Console.WriteLine("Enter File location:");
+            var fileLoction=Console.ReadLine();
+            IDatabaseFactory databaseStructure;
+            Console.WriteLine("Select a database(enter the number):\n1.MYSQL\n2.MONGODB\nEnter:");
+            var option = Console.Read();
+            databaseStructure = new SQLDatabaseContext();
+            databaseStructure.WriteToDatabase(fileLoction);
+            long ticks = sw.ElapsedTicks;
+            Console.WriteLine(ticks);
+            Console.Read();
+
         }
     }
 }
